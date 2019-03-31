c3ed8b24733c750705763ad8cc407c8438f5e021
esindger <esindger@gmail.com>
2019-03-31 17:57:57 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  extends: [
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ]
+}
---
+++ src/Index.ts
@@ -0,0 +1,7 @@
+export default class Index {
+  private limit = 1
+
+  public getLimit (): number {
+    return this.limit
+  }
+}
